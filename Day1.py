a62016e1157d0f9f942ed244f8c843a8f8e12b7b
Zain Shaikh <skzain007995@gmail.com>
2025-11-05 21:33:18 +0530

    learning pyton day 1 and 2

---
+++ Day1.py
@@ -0,0 +1,21 @@
+# print('hello world')
+# print("hello world")
+# print(''' Hi 
+#       my 
+#       name 
+#       is 
+#       zain''')
+# print (3 + 3) #output will be 6
+# print('3' + '3') #output will be 33 it will take both values as a string 
+# print('zain' + 'shaikh') #output will be zain shaikh
+# # print(4 + '5') #output will be error we cant add number and string 
+# print('zain ' * 4) #output will be zain zain zain zain there will be space cause i add space in the string 
+# print('4' * 5) #output will be 44444
+
+# # Variables
+
+# a = 10
+# print(a)
+
+
+
---
+++ Day2.py
@@ -0,0 +1,34 @@
+# this is an assignment operator it stores the right hand side variable value in the left hand side variable 
+# to compare both variable we use double equla to sign
+
+# a= 5
+# b= 2
+# a = b
+# print (a)
+# print (b)
+# a = a+b #gives the same ans as the line 10
+# a+=b
+# print (a)
+
+# input () takes input from the user 
+# input() takes every input in the form of string(imp)
+
+# name = input ("enter your name ")
+# fav_color = input ("enter your favourite color")
+# print (name , fav_color)     
+
+
+# num1 =int (input ("enter number")) to change the string input into integer input we use int 
+# num2 = int (input ("enter number")) to change the string input into integer input we use int
+# print (num1 + num2)
+
+# current_year = 2025
+# birth_year = int (input("enter your birth year"))
+# print (current_year - birth_year)
+
+# num = int(input ("enter a number"))
+# square = num * num
+# cube = square*num
+# print (square)
+# print (cube)
+
