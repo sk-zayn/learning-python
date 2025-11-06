7585692f4da9bd4cd71be135995e6ad7a23cd50e
Zain Shaikh <skzain007995@gmail.com>
2025-11-06 21:58:53 +0530

    2 more questions

--- Day3.py
+++ Day3.py
@@ -38,6 +38,40 @@
 # total =((quantity_of_tomato * tomato)+(quantity_of_beans * beans))
 # print(total)
 
+#QUESTION 5
+
+# max = 25
+# maximum_number = 25*6
+# sub1 = int (input("enter the marks of sub 1 0-25 "))
+# sub2 = int (input("enter the marks of sub 2 0-25 "))
+# sub3 = int (input("enter the marks of sub 3 0-25 "))
+# sub4 = int (input("enter the marks of sub 4 0-25 "))
+# sub5 = int (input("enter the marks of sub 5 0-25 "))
+# sub6 = int (input("enter the marks of sub 6 0-25 "))
+
+# subjects = sub1+sub2+sub3+sub4+sub5+sub6
+
+
+# if (sub1<=25) and (sub2<=25) and (sub3<=25) and (sub4<=25) and (sub5<=25) and (sub6<=25) :
+#     percentage = (subjects/maximum_number) * 100
+#     print(subjects)
+#     print(percentage)
+# else :
+#      print("please enter the correct number")
+
+
+#QUESTION 6
+# p = int (input ("enter the principle amount here "))
+# n = int (input ("enter no of years here "))
+# r = float (input("enter the rate of interest here "))
+
+# simple_interest = (p*n*r)/100
+# print(simple_interest)
+
+# total_amount = simple_interest + p
+# print(total_amount)
+
+
 
 
 
