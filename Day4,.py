e0ec79bb18b00b42c87a3ec11b48d93d292f33c8
Zain Shaikh <skzain007995@gmail.com>
2025-11-11 21:10:36 +0530

    Did some conditional stament practice

--- Day4,.py
+++ Day4,.py
@@ -1,34 +1,13 @@
 # In python there are two types of function (direct functions and imported function)
 
-import math
+# import math
 
-print(math.floor(80.2)) #.floor takes the lowest value 
-print (math.ceil(80.1)) #.ceil takes the highest value
+# print(math.floor(80.2)) #.floor takes the lowest value 
+# print (math.ceil(80.1)) #.ceil takes the highest value
 
-print(math.sqrt(25)) #gives you square root
-print(math.cbrt(64)) #gives you cube root
-print(math.factorial(5)) #gives you factorial 5*4*3*2*1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# print(math.sqrt(25)) #gives you square root
+# print(math.cbrt(64)) #gives you cube root
+# print(math.factorial(5)) #gives you factorial 5*4*3*2*1
 
 
 # Conditional Statement
@@ -69,5 +48,54 @@ print(math.factorial(5)) #gives you factorial 5*4*3*2*1
 #     print(num)
 # else :
 #     print("your number is less than 100")
+
+
+#Question 5
+# num = int (input("enter number here "))
+# if num % 2 == 0:
+#     print("It's even")
+# else:
+#     print("It's odd")
+
+#Question 6
+# num = int (input("enter number here "))
+
+# if num<0 :
+#     print("its a negative number")
+# elif num > 0 :
+#     print ("its a positive number")
+# else :
+#     print ("its a zero")
+    
+#Question 7
+# num = int (input("enter number here "))
+
+# if num < 100 :
+#     print("number is too small")
+# elif num > 100 :
+#     print ("number is too large")
+# else :
+#     print ("we think alike")
+
+#Question 8
+
+# max = 80
+# maximum_number = 80*4
+# sub1 = int (input("enter the marks of sub 1 "))
+# sub2 = int (input("enter the marks of sub 2 "))
+# sub3 = int (input("enter the marks of sub 3 "))
+# sub4 = int (input("enter the marks of sub 4 "))
+
+# subjects = sub1+sub2+sub3+sub4
+# percentage = (subjects/maximum_number) * 100
+
+# if (sub1<=80) and (sub2<=80) and (sub3<=80) and (sub4<=80):
+#     print(f"Total marks {subjects} / {maximum_number}")
+#     print(f"Percentage {percentage}")
+#     if percentage >= 80:
+#         print("very well done")
+# else :
+#     print("please enter the correct marks")
+
 
 
