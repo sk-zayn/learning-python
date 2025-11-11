ddbe1b5341046cbf086d3036e228aa7f1d7c3a4c
Zain Shaikh <skzain007995@gmail.com>
2025-11-11 21:18:22 +0530

    learned how slicing work for negative and positive index value

---
+++ Day5.py
@@ -0,0 +1,13 @@
+#slicing
+
+students = [ "alham", "zayn", "abuzar", "aquib", "saif", "mayank"]
+
+print (students[0:])
+print (students[:])
+print (students[0:3])
+print (students[2:4])
+print (students[1:5])
+print (students[3:5])
+print (students[-3:-3])
+print (students[1:-3])
+print (students[0:-4])
