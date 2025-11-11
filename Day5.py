55fa91a93774f394d8062bea79da1973e956b744
Zain Shaikh <skzain007995@gmail.com>
2025-11-11 21:27:29 +0530

    learned about reverse iteration

--- Day5.py
+++ Day5.py
@@ -1,13 +1,30 @@
 #slicing
+#negative value runs from right to left 
+
 
 students = [ "alham", "zayn", "abuzar", "aquib", "saif", "mayank"]
 
-print (students[0:])
-print (students[:])
-print (students[0:3])
+print (students[0:]) #print whole list 
+print (students[:]) #print whole list 
+print (students[0:3]) #from zero th index till 3 vlues
 print (students[2:4])
 print (students[1:5])
 print (students[3:5])
-print (students[-3:-3])
+print (students[-3:-3]) 
 print (students[1:-3])
 print (students[0:-4])
+
+
+#reverse iteration 
+students1 = [ "alham", "zayn", "abuzar", "aquib", "saif", "mayank"]
+print(students1 [:: -1]) # print every thing in reverse 
+print (students1 [::-2]) # print every thing in reverse with 1 value gap
+print(students1[::-3]) #print every thing in reverse with 2 value gap 
+
+#it also works on string 
+name = "zain"
+print(name[::-1])
+print(name[::-2])
+
+
+
