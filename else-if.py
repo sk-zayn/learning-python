ceab83278dc49212f0d387ab441aac3d7a2184b1
Zain Shaikh <skzain007995@gmail.com>
2025-11-13 20:59:27 +0530

    did 1 more question

--- else-if.py
+++ else-if.py
@@ -1,19 +1,47 @@
 #Practice question 1
-age = int (input ("enter your age "))
+# age = int (input ("enter your age "))
 
-if age >= 60:
-    print("Senior citizen")
-elif age >=18 :
-    print("Adult")
-else:
-    print("Minor")
+# if age >= 60:
+#     print("Senior citizen")
+# elif age >=18 :
+#     print("Adult")
+# else:
+#     print("Minor")
 
-# Question 6
-num = int (input("enter number here "))
+# Question 2
+# num = int (input("enter number here "))
 
-if num < 0 :
-    print("its a negative number")
-elif num > 0 :
-    print ("its a positive number")
-else :
-    print ("its a zero")
+# if num < 0 :
+#     print("its a negative number")
+# elif num > 0 :
+#     print ("its a positive number")
+# else :
+#     print ("its a zero")
+
+
+#Question 3
+
+# max = 80
+# maximum_number = 80*4
+# sub1 = int (input("enter the marks of sub 1 "))
+# sub2 = int (input("enter the marks of sub 2 "))
+# sub3 = int (input("enter the marks of sub 3 "))
+# sub4 = int (input("enter the marks of sub 4 "))
+
+# subjects = sub1+sub2+sub3+sub4
+# percentage = (subjects/maximum_number) * 100
+
+# if (sub1<=80) and (sub2<=80) and (sub3<=80) and (sub4<=80):
+#     print(f"Total marks {subjects} / {maximum_number}")
+#     print(f"Percentage {percentage}")
+#     if percentage >= 80:
+#         print("Distinction")
+#     elif percentage >= 60:
+#         print("First class")
+#     elif percentage >= 40:
+#         print("Pass")
+#     elif percentage < 34:
+#         print("Fail")
+#     else :
+#         print("please enter the correct marks")
+
