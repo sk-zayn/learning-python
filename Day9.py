8d1bdb1ad269f2fa19357003a269faa9554431f3
Zain Shaikh <skzain007995@gmail.com>
2025-11-18 20:53:50 +0530

    for loop

---
+++ Day9.py
@@ -0,0 +1,12 @@
+list = ["zain", "zaid", "zainab", "zahid", "parveen"]
+for name in list : #name is iterator and list is iterable, iterator visits iterable and perform the given function until the list is over.
+  print(name)
+else : #in python we can use else of loops also it will run when the condition goes false like here it will print every naem in the list and when list is over it will print else statement.
+  print("list is over")
+
+
+#Range it takes 3 parameter 1st starting point 2nd ending point 3rd gap between them.
+
+for i in range(1, 51, 2) :# goes from 1 to 50 and leave 1 number in between 1,3,5,7,9...49
+  print(i)
+
