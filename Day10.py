92f211fa73beb31e7dcee4bd82c4a8828143a20d
Zain Shaikh <skzain007995@gmail.com>
2025-11-19 20:33:41 +0530

    Break in loops

--- Day10.py
+++ Day10.py
@@ -3,3 +3,11 @@ for i in range(1,6):
 else :
   print ("done")
 
+list1 = ["zain", "zahid", "zainab", "parveen", "zaid"]
+
+for i in list1 :
+  if i == "zaid" :
+    break # when looops breaks else statement wont run 
+  print (i)
+else :
+  print ("list is over")
