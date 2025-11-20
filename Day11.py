e254cbf9f79084fba06bcd0546f81bd7273eb1e4
Zain Shaikh <skzain007995@gmail.com>
2025-11-20 21:06:46 +0530

    Practiced for loop question

--- Day11.py
+++ Day11.py
@@ -7,3 +7,25 @@ for i in range (1, 4):
     break
 else:
   print("You've Lost")
+
+fruits = ["apple", "banana", "guava", "cherry"]
+adj = ["bitter", "sour", "sweet"]
+
+for i in fruits:
+  for j in adj:
+    print(i,j)
+  else:
+    print("Inner loop is over")
+else:
+  print("Outer loop is over")
+
+
+for i in range (1,11):
+  for j in range(1,i):
+    print (j, end="")
+  print()
+
+list = [[1, 2, 3,], [4, 5, 6,], [7, 8, 9]]
+for i in list:
+  for j in i:
+    print (j)
