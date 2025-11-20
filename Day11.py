b05f5e21ba904dfd76fb108e9d88bf76b95bf37a
Zain Shaikh <skzain007995@gmail.com>
2025-11-20 21:11:20 +0530

    Intro to while loop

--- Day11.py
+++ Day11.py
@@ -1,31 +1,37 @@
-num = 12
+# num = 12
 
-for i in range (1, 4):
-  guess = int(input("Enter your guess "))
-  if guess == num :
-    print("You Won")
-    break
-else:
-  print("You've Lost")
+# for i in range (1, 4):
+#   guess = int(input("Enter your guess "))
+#   if guess == num :
+#     print("You Won")
+#     break
+# else:
+#   print("You've Lost")
 
-fruits = ["apple", "banana", "guava", "cherry"]
-adj = ["bitter", "sour", "sweet"]
+# fruits = ["apple", "banana", "guava", "cherry"]
+# adj = ["bitter", "sour", "sweet"]
 
-for i in fruits:
-  for j in adj:
-    print(i,j)
-  else:
-    print("Inner loop is over")
-else:
-  print("Outer loop is over")
+# for i in fruits:
+#   for j in adj:
+#     print(i,j)
+#   else:
+#     print("Inner loop is over")
+# else:
+#   print("Outer loop is over")
 
 
-for i in range (1,11):
-  for j in range(1,i):
-    print (j, end="")
-  print()
+# for i in range (1,11):
+#   for j in range(1,i):
+#     print (j, end="")
+#   print()
 
-list = [[1, 2, 3,], [4, 5, 6,], [7, 8, 9]]
-for i in list:
-  for j in i:
-    print (j)
+# list = [[1, 2, 3,], [4, 5, 6,], [7, 8, 9]]
+# for i in list:
+#   for j in i:
+#     print (j)
+
+
+i = 1
+while i<=5 :
+  print(i)
+  i = i+1
