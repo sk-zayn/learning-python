f5d7af42530fc8b5843aff89f4eb4e0302115f88
Zain Shaikh <skzain007995@gmail.com>
2025-11-26 15:53:35 +0530

    Made folders and move files in them to keep my learning content seperate

---
+++ Abdul-Bari/AB-Strings.py
@@ -0,0 +1,10 @@
+# Any thing that is written in quotes can be called string 
+# Operators in string
+'''
+1. concatination
+2. Repetition
+3. indexing
+4. slicing
+5. in 
+6. not in
+'''
--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-
-# output = ''
-# for i in range(1,5):
-#   output = output + str(random.randint(1,9))
-# print(output)
