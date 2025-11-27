a8492f13fc13f5f5e4f3ce3f2506b63567cfaa9e
Zain Shaikh <skzain007995@gmail.com>
2025-11-27 20:48:30 +0530

    printed type for multiple data type

---
+++ Iqra-class/index.py
@@ -0,0 +1,15 @@
+print('Hello World')
+print("Shaikh Zain")
+print("Bsc.IT")
+
+a = 10
+b = 'hello'
+c = True
+d = 10.2
+value = None
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(value))
