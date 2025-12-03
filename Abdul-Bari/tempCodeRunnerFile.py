4ce913936025695e90612df5cc8ea229b5e00666
Zain Shaikh <skzain007995@gmail.com>
2025-12-03 15:26:28 +0530

    delete file

--- Abdul-Bari/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-string = input('Enter a string here: ')
-# s = string[:-1]
-# print(s)
-
-
-# if string[:] == string[::-1]:
-#   print('Your sting is palindrome ')
-# else:
-#   print('Your string is not a palindrome')
