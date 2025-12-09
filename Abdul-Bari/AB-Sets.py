e955a5db3cae99086b9176fbabc8cfd4be3649ee
Zain Shaikh <skzain007995@gmail.com>
2025-12-09 14:33:25 +0530

    Sets

---
+++ Abdul-Bari/AB-Sets.py
@@ -0,0 +1,25 @@
+set = {1, 2, 3,'zain', True, 40.3, False, True}
+print(set)
+
+set2 = {True, False}
+print(set2)
+
+#Formula for hashing = h(x) = x % 10
+#set values use this formula to get their place in array
+x = 5 #hx = x % 10
+print(10%16)  # will get stored at index 5
+x = 10 #hx = x % 10
+print(20%16) # will get stored at index 0
+x = 21 #hx = x % 10
+print(30%16) # will get stored at index 1
+x = 15 #hx = x % 10
+print(40%16) # will get stored at index 5
+x = 3 #hx = x % 10
+print(50%16) # will get stored at index 3
+x = 11 #hx = x % 10
+print(60%16) # will get stored at index 1
+
+set1= {10, 20, 30, 40, 50, 60}
+print(set1)
+
+
