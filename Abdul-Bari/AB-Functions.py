3744b64f505307f38ad52ece31f82c202b1b5fca
Zain Shaikh <skzain007995@gmail.com>
2025-12-12 15:05:36 +0530

    unpaking arguments

--- Abdul-Bari/AB-Functions.py
+++ Abdul-Bari/AB-Functions.py
@@ -1,5 +1,5 @@
 #Default parameter
-# it works from right hand side means if you want to give only one default parameter it should be from the right hand side 
+# it works from right hand side means if you want to give only one default parameter it should be from the right hand side
 def add (a=1, b=5, c=2):
   print(a + b + c)
 
@@ -10,5 +10,19 @@ def add (a, b, c):
   print(a + b + c)
 
 add(10, c=2, b=5)
+
+#unpacking arguments
+def printing(a, b, c):
+  print(a, b, c)
+
+l1 = [1, 2, 3, 4, 5]
+printing(l1[1], l1[4], l1[2]) #using index to pass the iterable value in the function parrameter
+
+# printing(*l1)#using * to iterate values and pass the values in the function parameter
+# only if the iterable value has the same no of elements as the parameters
+
+
+
+
 
 
