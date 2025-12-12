d0c601297f860aa0c5953a5849506036d82c4bbc
Zain Shaikh <skzain007995@gmail.com>
2025-12-12 21:10:09 +0530

    practicing different types of arguments in function

--- Abdul-Bari/AB-Functions.py
+++ Abdul-Bari/AB-Functions.py
@@ -3,7 +3,7 @@
 def add (a=1, b=5, c=2):
   print(a + b + c)
 
-add(7)
+add()
 
 #positional + keyword arguments (positional arguments need to be before keyword arguments)
 def add (a, b, c):
@@ -11,9 +11,14 @@ def add (a, b, c):
 
 add(10, c=2, b=5)
 
+
 #unpacking arguments
-def printing(a, b, c):
-  print(a, b, c)
+def printing(a, b, *c): # using * to give more than one value to the variable and it will be in the form of tuple
+  return a, b, c
+
+print(printing(10, 20, 30, 40, 50, 50, 4, 3, 2, 1)) #a and b are at the starting so we have to give positional values to those
+# if a had the star then we would have to give keyword argument to b and c (these are the positional arguments which will returns us tuple)
+
 
 l1 = [1, 2, 3, 4, 5]
 printing(l1[1], l1[4], l1[2]) #using index to pass the iterable value in the function parrameter
@@ -21,8 +26,18 @@ printing(l1[1], l1[4], l1[2]) #using index to pass the iterable value in the fun
 # printing(*l1)#using * to iterate values and pass the values in the function parameter
 # only if the iterable value has the same no of elements as the parameters
 
+def fun2(**kwargs): #we use ** to give as many args as we want but in keyword form
+  print(kwargs)
+
+fun2(name= 'zain', roll= 20, add= 'mumbai') #using keyword arguments will give us dictionary
 
 
 
+def add(a, b, c):
+  x, y, z =1, 2, 3
+  print(locals())
 
+add(5, 6, 7)
+
+print(globals())
 
