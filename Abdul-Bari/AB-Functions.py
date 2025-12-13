37179356425e4890e080825d064a69375b4f66bc
Zain Shaikh <skzain007995@gmail.com>
2025-12-13 11:40:54 +0530

    Pascal tringle

--- Abdul-Bari/AB-Functions.py
+++ Abdul-Bari/AB-Functions.py
@@ -41,73 +41,135 @@
 
 # print(globals())
 
-def maximum_limit(a, b):
-  if abs(a-b) <= 5 :
-    return True
+# def maximum_limit(a, b):
+#   if abs(a-b) <= 5 :
+#     return True
+#   else:
+#     return False
+
+# print(maximum_limit(2, 4))
+
+
+# def largest_number(a, b, c):
+#   if a > b and a > c:
+#     return f'{a} is the largest number'
+#   elif b > a and b > c:
+#     return f'{b} is the largest number'
+#   else:
+#     return f'{c} is the largest number'
+
+# print(largest_number(4, 6, 2))
+# print(largest_number(2, 4, 8))
+# print(largest_number(8, 4, 2))
+
+# def intro(name, profession, /):# back slash is use when you want the parameter to be positional only
+# # when slash is there you cant give keyword parameters
+#   return f'Hii my name is {name} and i am a {profession}'
+
+# print(intro('zain', 'student'))
+
+
+# def planet_finder(id):
+#   planets = {
+#     1 : 'Mercury',
+#     2 : 'Venus',
+#     3 : 'Earth',
+#     4 : 'Mars',
+#     5 : 'Jupiter',
+#     6 : 'Saturn',
+#     7 : 'Uranus',
+#     8 : 'Neptune',
+#     9 : 'Pluto'
+#   }
+#   if id > 0 and id < 9:
+#     return planets[id]
+#   else:
+#     return 'invalid id'
+
+# id = int(input('Enter the planet id here: '))
+# p = planet_finder(id)
+# print('your planet is ', p)
+
+# l1 = [100, 545, 340, 345, 678, 32, 20, 600]
+
+# def add_zeros(l1):
+#   sum = 0
+#   for i in l1:
+#     if i % 10 == 0:
+#       sum += i
+#   return sum
+
+# print(add_zeros(l1))
+
+# d = {'a':10, 'b':20, 'c':30, 'd':40}
+
+# def invert (d):
+#   newd = {}
+#   for k, v in d.items():
+#     newd[v] = k
+#   return newd
+
+
+# print(invert(d))
+
+# set1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# def pangram(sen):
+#   set2 = set(sen)
+#   set2.discard(' ')
+#   set3 = sorted(set2)
+#   print(set3)
+#   if set3 == set1:
+#     return 'The given sentence is pangram'
+#   else:
+#     return 'The given sentence is not a pangram'
+
+# print(pangram('the quick brown fox jumps over the lazy dog'))
+
+
+sentence = ('My Name Is Zain Zahid Shaikh FDERT')
+# def case_count (sentence):
+#   sen1 = sentence.replace(' ','')
+#   lower_case = ''
+#   upper_case = ''
+#   for i in sen1:
+#     if i.islower() == True:
+#       lower_case += i
+#     else:
+#       upper_case += i
+#   return f'count of lower case is {len(lower_case)}, count of upper case is {len(upper_case)}'
+
+# print(case_count(sentence))
+
+# def count (sen):
+#   upper = 0
+#   lower = 0
+#   for i in sen:
+#     if i.islower() == True:
+#       lower += 1
+#     elif i.isupper() == True:
+#       upper += 1
+#   return lower, upper
+
+# print(count(sentence))
+
+def smallest (*args, low_limit = 0):
+  if low_limit == None:
+    return min(args)
   else:
-    return False
+    list =[x for x in args if x >= low_limit]
+    return min(list)
 
-print(maximum_limit(2, 4))
+print(smallest(5, 3, 4, 7))
+print(smallest(1, 3, 5, -6, 4))
 
+def pascal(n):
+  r = [1]
 
-def largest_number(a, b, c):
-  if a > b and a > c:
-    return f'{a} is the largest number'
-  elif b > a and b > c:
-    return f'{b} is the largest number'
-  else:
-    return f'{c} is the largest number'
+  for i in range(n):
+    print(r)
+    tr = [0] + r
+    r = r + [0]
+    nr = [x+y for x, y in zip(r, tr)]
+    r = nr
 
-print(largest_number(4, 6, 2))
-print(largest_number(2, 4, 8))
-print(largest_number(8, 4, 2))
-
-def intro(name, profession, /):# back slash is use when you want the parameter to be positional only
-# when slash is there you cant give keyword parameters
-  return f'Hii my name is {name} and i am a {profession}'
-
-print(intro('zain', 'student'))
-
-
-def planet_finder(id):
-  planets = {
-    1 : 'Mercury',
-    2 : 'Venus',
-    3 : 'Earth',
-    4 : 'Mars',
-    5 : 'Jupiter',
-    6 : 'Saturn',
-    7 : 'Uranus',
-    8 : 'Neptune',
-    9 : 'Pluto'
-  }
-  if id > 0 and id < 9:
-    return planets[id]
-  else:
-    return 'invalid id'
-
-id = int(input('Enter the planet id here: '))
-p = planet_finder(id)
-print('your planet is ', p)
-
-l1 = [100, 545, 340, 345, 678, 32, 20, 600]
-
-def add_zeros(l1):
-  sum = 0
-  for i in l1:
-    if i % 10 == 0:
-      sum += i
-  return sum
-
-print(add_zeros(l1))
-
-d = {'a':10, 'b':20, 'c':30, 'd':40}
-
-def invert (d):
-  newd = {}
-  for k, v in d.items():
-    newd[v] = k
-  return newd
-
-
-print(invert(d))
+pascal(8)
