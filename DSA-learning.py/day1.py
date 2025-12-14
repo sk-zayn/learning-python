74d416193bcedab1c9c77bbe65b721d53505b1a4
Zain Shaikh <skzain007995@gmail.com>
2025-12-14 19:58:28 +0530

    simple calculator

--- DSA-learning.py/day1.py
+++ DSA-learning.py/day1.py
@@ -1,22 +1,38 @@
-print('Hello, World!')
-a, b, c, d, e = 10, 30.4, 'zain', True, 34+6j
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
-print(type(e))
+# print('Hello, World!')
+# a, b, c, d, e = 10, 30.4, 'zain', True, 34+6j
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+
+# num1 = int(input('Enter a number here: '))
+# num2 = int(input('Enter a number here: '))
+# print(num1 + num2, num1 - num2)
+
+# year = int(input('Enter year here: '))
+# if year % 4 == 0 and year % 100 != 0:
+#   print('Its a leap year')
+# elif year % 400 == 0:
+#   print('Its a leap year')
+# else:
+#   print('Its not a leap year')
 
 num1 = int(input('Enter a number here: '))
+num3 = input('Enter the operator you want to perform on numbers: ')
 num2 = int(input('Enter a number here: '))
-print(num1 + num2, num1 - num2)
 
-year = int(input('Enter year here: '))
-if year % 4 == 0 and year % 100 != 0:
-  print('Its a leap year')
-elif year % 400 == 0:
-  print('Its a leap year')
-else:
-  print('Its not a leap year')
+if num3 == '*':
+  print(num1 * num2)
+elif num3 == '+':
+  print(num1 + num2)
+elif num3 == '-':
+  print(num1 - num2)
+elif num3 == '/':
+  print(num1 / num2)
+
+
+
 
 
 
