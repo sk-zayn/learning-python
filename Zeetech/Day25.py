e265ee979f4534122e61b6b05baf48087608f169
Zain Shaikh <skzain007995@gmail.com>
2025-12-29 00:06:49 +0530

    add function to find common elements between two lists

--- Zeetech/Day25.py
+++ Zeetech/Day25.py
@@ -39,3 +39,18 @@ lambo = car()
 lambo.run()
 
 
+# Write a function to find common elements between two lists.
+
+l1 = [1, 2, 3, 4, 5, 6]
+l2 = [5, 6, 7, 8, 9, 2]
+
+l3 = []
+def commonNumber(l1, l2):
+  for i in l1:
+    if i in l2 :
+      l3.append(i)
+  return(l3)
+
+print(commonNumber(l1, l2))
+
+
