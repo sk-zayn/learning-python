2fb7ee18e6b57b2739f2ad83c414a06c975aa207
sk-zayn <skzain007995@gmail.com>
2026-01-21 14:11:06 +0530

    add Circle class with area and parameter methods

--- Abdul-Bari/OOPS-Challenges.py
+++ Abdul-Bari/OOPS-Challenges.py
@@ -1,4 +1,5 @@
 from random import *
+import math
 class Dice:
   def __init__(self, sides = 6):
     self.sides = sides
@@ -12,4 +13,21 @@ print(d.roll_dice())
 print(d.roll_dice())
 
 
+class Circle:
+  def __init__(self, radius):
+    self.radius = radius
 
+  def area(self):
+    area = math.pi * self.radius * self.radius
+    return area
+
+  def parameter(self):
+    parameter = 2 * math.pi * self.radius
+    return parameter
+
+
+c = Circle(8)
+
+print(c.area())
+print(c.parameter())
+print(math.pi)
